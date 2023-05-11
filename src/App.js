bb3ddd98c1c5141641f0e7aaef15945d4a93e98f
Lakee Rawat <73211332+lakeerawat@users.noreply.github.com>
2023-05-11 16:36:27 +0530

    Update App.js
    comments deleted

--- src/App.js
+++ src/App.js
@@ -2,27 +2,7 @@ import './App.css';
 import AllRoutes from './pages/AllRoutes';
 
 function App() {
-  // const [movies, setMovies] = useState([]);
-  // const [movie, setMovie] = useState([]);
-  
-
-  // const genres=["Drama","Science-Fiction","Thriller","Crime","Comedy"];
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await axios.get(`${BASE_URL}/shows`);
-  //     setMovies(res.data);
-  //   };
-  //   fetchData();
-  // }, []);
-  
-
-  // const getMovie = async id => {
-  //   const res = await axios.get(`${BASE_URL}/shows/${id}`);
-  //   setMovie(res.data);
-  // };
- 
-  return (
+   return (
     <div className="App">
       <AllRoutes/>
     </div>
